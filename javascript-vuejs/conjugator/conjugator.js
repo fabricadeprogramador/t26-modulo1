527060fc832966a20ca5ca4b3ede94c2d6abc724
Virmerson Bento dos Santos <virmerson@hightechcursos.com.br>
2019-09-21 14:06:23 -0400

    Aula dia 21/09

--- javascript-vuejs/conjugator/conjugator.js
+++ javascript-vuejs/conjugator/conjugator.js
@@ -1,11 +1,59 @@
 var app = new Vue({
     el: '#app',
     data: {
-        verbos: [{
-            base: 'work',
-            present: 'works',
-            portugues: 'Trabalhar',
-            simplePast: 'worked'
-        }]
+        verbs: [{
+                base: 'work',
+                thirdPersonSingular: 'works',
+                simplePast: 'worked',
+                pastParticiple: 'worked',
+                continuous: 'working',
+                definition: "be engaged in physical or mental activity in order to achieve a purpose or result, especially in one's job; do work",
+                examples: ['He said it would work for a girl or a boy.',
+                    'A coloring book and crayons kept her busy while they worked and talked.',
+                    'We had worked hard on a presentation to a potential customer.'
+                ]
+            },
+
+            {
+                base: 'eat',
+                thirdPersonSingular: 'eats',
+                simplePast: 'ate',
+                pastParticiple: 'eaten',
+                continuous: 'eating',
+                definition: "the action of ingesting food",
+                examples: ['He eats pizza every friday',
+                    'They have been eating a lot lately'
+                ]
+            },
+
+            {
+                base: 'eat',
+                thirdPersonSingular: 'eats',
+                simplePast: 'ate',
+                pastParticiple: 'eaten',
+                continuous: 'eating',
+                definition: "the action of ingesting food",
+                examples: ['He eats pizza every friday',
+                    'They have been eating a lot lately'
+                ]
+            },
+            {
+                base: 'eat',
+                thirdPersonSingular: 'eats',
+                simplePast: 'ate',
+                pastParticiple: 'eaten',
+                continuous: 'eating',
+                definition: "the action of ingesting food",
+                examples: ['He eats pizza every friday',
+                    'They have been eating a lot lately'
+                ]
+            }
+        ]
+    },
+    computed: {
+        verb: function () {
+            return this.verbs[1];
+        }
+
     }
 });
