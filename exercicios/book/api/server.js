e8d5c8c68ce7c197f69facdb1f9751fa9e853e82
Virmerson Bento dos Santos <virmerson@hightechcursos.com.br>
2019-11-16 15:43:19 -0400

    Iniciando com mongo

--- exercicios/book/api/server.js
+++ exercicios/book/api/server.js
@@ -11,3 +11,38 @@ app.use('/categoria', categoriaRoutes)
 //Configurando a porta
 const port = 3000
 app.listen(port, () => console.log(`Api rodando na porta ${port}!`))
+
+//Conectando com Mongo
+
+var mongoose = require('mongoose');
+mongoose.connect(
+    'mongodb+srv://appfrases:12345@cluster0-v20sn.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("Conectado com sucesso no Mongo")
+});
+
+// Criando Schema
+
+var categoriaSchema = new mongoose.Schema({
+    descricao: String
+});
+//Criando o modelo 
+var Categoria = mongoose.model('Categoria', categoriaSchema);
+
+//Instanciando um objeto
+var cat = new Categoria({
+    descricao: 'Viagem'
+})
+//Faz a persistencia
+cat.save(function (error, catSalva) {
+    if (error) {
+        console.log("Nao salvou")
+    }
+
+    console.log(catSalva._id)
+})
